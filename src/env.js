19d65a958f822f36d0493c35b5ae22d4141d73c3
hammadtariq838 <24100267@lums.edu.pk>
2024-07-31 00:44:56 +0500

    feat: environment variables schema defined

--- src/env.js
+++ src/env.js
@@ -4,18 +4,24 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    AUTH_SECRET: z.string().optional(),
+    AUTH_SECRET: z.string(),
     NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
+    RESEND_API_KEY: z.string(),
+    RESEND_DOMAIN: z.string(),
   },
 
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_APP_URL: z.string().url(),
   },
 
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     AUTH_SECRET: process.env.AUTH_SECRET,
     NODE_ENV: process.env.NODE_ENV,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    RESEND_DOMAIN: process.env.RESEND_DOMAIN,
+
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
   },
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -1,33 +1,28 @@
-import { Resend } from 'resend';
+import { env } from "@/env";
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(env.RESEND_API_KEY);
 
-const domain = process.env.NEXT_PUBLIC_APP_URL;
+const domain = env.NEXT_PUBLIC_APP_URL;
 
-export const sendPasswordResetEmail = async (
-  email: string,
-  token: string
-) => {
+export const sendPasswordResetEmail = async (email: string, token: string) => {
   const resetLink = `${domain}/auth/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: process.env.RESEND_DOMAIN!,
+    from: env.RESEND_DOMAIN!,
     to: email,
-    subject: 'Reset your password',
+    subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`,
   });
 };
 
-export const sendVerificationEmail = async (
-  email: string,
-  token: string
-) => {
+export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: process.env.RESEND_DOMAIN!,
+    from: env.RESEND_DOMAIN!,
     to: email,
-    subject: 'Confirm your email',
+    subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
   });
 };
